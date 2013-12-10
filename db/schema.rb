158adafd14a8cbb7dfe215607012c836e49bd30d
Armando Serrano <aserranox89@gmail.com>
2013-12-09 22:33:16 -0700

    Update to item creation
    added price and image posting from url

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131210042101) do
+ActiveRecord::Schema.define(version: 20131210044502) do
 
   create_table "products", force: true do |t|
     t.string   "title"
